e0f87e555196cd54b2538f97f972c0a264888628
John Pham <john@highlight.run>
2021-10-01 18:19:28 -0700

    Cleanup

--- main.py
+++ main.py
@@ -5,61 +5,65 @@ import cv2
 
 #== Parameters =======================================================================
 BLUR = 21
-CANNY_THRESH_1 = 100
+CANNY_THRESH_1 = 10
 CANNY_THRESH_2 = 200
-MASK_DILATE_ITER = 8
-MASK_ERODE_ITER = 15
-MASK_COLOR = (0.0,0.0,0.0) # In BGR format
+MASK_DILATE_ITER = 10
+MASK_ERODE_ITER = 10
+MASK_COLOR = (0.0,0.0,1.0) # In BGR format
 
+def maskForeground(inputPath, outputPath):
+	img = cv2.imread(inputPath)
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-img = cv2.imread('./assets/sample-photos/elon.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	#-- Edge detection -------------------------------------------------------------------
+	edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
+	edges = cv2.dilate(edges, None)
+	edges = cv2.erode(edges, None)
 
-#-- Edge detection -------------------------------------------------------------------
-edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
-edges = cv2.dilate(edges, None)
-edges = cv2.erode(edges, None)
+	#-- Find contours in edges, sort by area ---------------------------------------------
+	contour_info = []
+	contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+	for c in contours:
+		contour_info.append((
+			c,
+			cv2.isContourConvex(c),
+			cv2.contourArea(c),
+		))
+	contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+	max_contour = contour_info[0]
 
-#-- Find contours in edges, sort by area ---------------------------------------------
-contour_info = []
-contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-for c in contours:
-	contour_info.append((
-		c,
-		cv2.isContourConvex(c),
-		cv2.contourArea(c),
-	))
-contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
-max_contour = contour_info[0]
+	#-- Create empty mask, draw filled polygon on it corresponding to largest contour ----
+	# Mask is black, polygon is white
+	mask = np.zeros(edges.shape)
+	cv2.fillConvexPoly(mask, max_contour[0], (255))
 
-#-- Create empty mask, draw filled polygon on it corresponding to largest contour ----
-# Mask is black, polygon is white
-mask = np.zeros(edges.shape)
-cv2.fillConvexPoly(mask, max_contour[0], (255))
+	#-- Smooth mask, then blur it --------------------------------------------------------
+	mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
+	mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
+	mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
+	mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
 
-#-- Smooth mask, then blur it --------------------------------------------------------
-mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
-mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
-mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
-mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
+	#-- Blend masked img into MASK_COLOR background --------------------------------------
+	mask_stack  = mask_stack.astype('float32') / 255.0          # Use float matrices,
+	img         = img.astype('float32') / 255.0                 #  for easy blending
 
-#-- Blend masked img into MASK_COLOR background --------------------------------------
-mask_stack  = mask_stack.astype('float32') / 255.0          # Use float matrices,
-img         = img.astype('float32') / 255.0                 #  for easy blending
+	masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR) # Blend
+	masked = (masked * 255).astype('uint8')                     # Convert back to 8-bit
 
-masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR) # Blend
-masked = (masked * 255).astype('uint8')                     # Convert back to 8-bit
+	c_red, c_green, c_blue = cv2.split(img)
+	img_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
+	cv2.imwrite(outputPath, img_a*255)
 
-c_red, c_green, c_blue = cv2.split(img)
-img_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
-cv2.imwrite('./out/masked.png', img_a*255)
+def cropToFace(inputPath, outputPath):
+	cropper = Cropper(width=80, height=100, face_percent=100)
 
-cropper = Cropper(width=80, height=100, face_percent=100)
+	# Get a Numpy array of the cropped image
+	cropped_array = cropper.crop(inputPath)
 
-# Get a Numpy array of the cropped image
-cropped_array = cropper.crop('./out/masked.png')
+	# Save the cropped image with PIL if a face was detected:
+	if len(cropped_array) > 0:
+		cropped_image = Image.fromarray(cropped_array)
+		cropped_image.save(outputPath)
 
-# Save the cropped image with PIL if a face was detected:
-if len(cropped_array) > 0:
-    cropped_image = Image.fromarray(cropped_array)
-    cropped_image.save('./out/cropped.png')
+maskForeground('./assets/sample-photos/john.png', './out/masked.png')
+cropToFace('./out/masked.png', './out/cropped.png')
